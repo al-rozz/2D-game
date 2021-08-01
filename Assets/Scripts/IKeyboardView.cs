d9b33fb37791e70be5ca07db6879cf8e9f98db17
toxicmouse7 <utiha@sasuke.ru>
2021-08-01 20:42:56 +0500

    Initial commit

---
+++ Assets/Scripts/IKeyboardView.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void KeyPressedHandler();
+public interface IKeyboardView
+{
+    event KeyPressedHandler attackPressed;
+}
---
+++ Assets/Scripts/KeyboardView.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "keyboard_settings", menuName = "Settings/Keyboard Settings")]
+public class KeyboardView : MonoBehaviour, IKeyboardView
+{
+    public KeyCode attackButton { get; private set; }
+    public KeyCode moveLeftButton { get; set; }
+    public KeyCode moveRightButton { get; set; }
+    public KeyCode jumpButton { get; set; }
+
+    public event KeyPressedHandler attackPressed;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(attackButton))
+        {
+            attackPressed();
+        }
+
+        if (Input.GetKey(moveLeftButton))
+        {
+            
+        }
+
+        if (Input.GetKey(moveRightButton))
+        {
+            
+        }
+
+        if (Input.GetKeyDown(jumpButton))
+        {
+            
+        }
+    }
+}
